78890bedeb49aef8b821cab641470fa4a23e1834
ok97 <omkhawshi0@gmail.com>
2021-05-23 18:48:52 +0530

    Added: Create new table

---
+++ AddressBookSystem-LINQ/AddressBookTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem_LINQ
+{
+    class AddressBookTable
+    {  
+        DataTable table = new DataTable("AddressBookSystem"); // UC 1 Create a new DataTable
+    }
+}
---
+++ AddressBookSystem-LINQ/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Address Book Data LINQ ");
+
+            // UC 1 Create a new table
+            AddressBookTable addressBookTable = new AddressBookTable();
+        }
+    }
+}
