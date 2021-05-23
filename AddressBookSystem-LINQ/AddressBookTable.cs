5a521a1a9c3a760ffae63945d332326457e032b8
ok97 <omkhawshi0@gmail.com>
2021-05-23 18:52:25 +0530

    Added: Adding Attributes

--- AddressBookSystem-LINQ/AddressBookTable.cs
+++ AddressBookSystem-LINQ/AddressBookTable.cs
@@ -10,5 +10,21 @@ namespace AddressBookSystem_LINQ
     class AddressBookTable
     {  
         DataTable table = new DataTable("AddressBookSystem"); // UC 1 Create a new DataTable
+        /* UC2:- Ability to create a Address Book Table with first and last names, address, city, 
+                 state, zip, phone number and email as its attributes
+        */
+        public AddressBookTable()
+        {
+            table.Columns.Add("FirstName", typeof(string));
+            table.Columns.Add("LastName", typeof(string));
+            table.Columns.Add("Address", typeof(string));
+            table.Columns.Add("City", typeof(string));
+            table.Columns.Add("State", typeof(string));
+            table.Columns.Add("Zip", typeof(string));
+            table.Columns.Add("PhoneNumber", typeof(string));
+            table.Columns.Add("Email", typeof(string));
+        }
+
+
     }
 }
